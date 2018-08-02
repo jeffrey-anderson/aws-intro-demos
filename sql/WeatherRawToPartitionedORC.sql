ed43f15b3d78ce91f34a48fcc37f9038f58bd084
jeff <jeff@jeff-anderson.com>
2018-08-01 21:21:29 -0400

    Added script to create S3 external weather data table partitioned by station, year, month

---
+++ sql/WeatherRawToPartitionedORC.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS weather;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS weather.noaa_ghcn_daily (
+  `id` string,
+  `obs_date` string,
+  `ELEMENT` string,
+  `ELEMENT_DATA` string,
+  `M_FLAG` string,
+  `Q_FLAG` string,
+  `S_FLAG` string,
+  `OBS_TIME` string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://noaa-ghcn-pds/csv.gz/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS weather.noaa_ghcn_daily_pid_ym (
+  obs_date date,
+  ELEMENT string,
+  ELEMENT_DATA int,
+  M_FLAG string,
+  Q_FLAG string,
+  S_FLAG string,
+  OBS_TIME string
+)
+PARTITIONED BY (id string, year int, month int)
+STORED AS ORC
+LOCATION 's3://YOUR-BUCKET-NAME-HERE/noaa_ghcn_daily/orc';
+
+INSERT OVERWRITE TABLE weather.noaa_ghcn_daily_pid_ym PARTITION (id='USW00014821', year, month)
+   SELECT cast(concat(substr(OBS_DATE,1,4),'-',substr(OBS_DATE,5,2),'-',substr(OBS_DATE,7,2)) as DATE),
+   ELEMENT,
+   CAST(ELEMENT_DATA as int),
+   M_FLAG, Q_FLAG, S_FLAG, OBS_TIME,
+   cast (substr(OBS_DATE,1,4) as int),
+   cast (substr(OBS_DATE,5,2) as int)
+   FROM weather.noaa_ghcn_daily
+   where ID = 'USW00014821';
